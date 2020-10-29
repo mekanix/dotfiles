5d4f9facae2c07c2c21ef30820053fba288db0d3
Goran Mekić <meka@tilda.center>
2020-10-29 23:50:44 +0100

    Update vim config

--- bin/browser.sh
+++ bin/browser.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Firefox STUN/TURN debug output
+# export R_LOG_LEVEL=9
+# export R_LOG_DESTINATION=stderr
+# export R_LOG_VERBOSE=1
+
 # exec chrome --incognito --disable-infobars $@ &
 # exec iridium --incognito --disable-infobars $@ &
 exec firefox $@ &
