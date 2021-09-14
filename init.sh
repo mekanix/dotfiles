4404d9c97c19c72adf9a4b34206430c698d3cb19
Goran Mekić <meka@tilda.center>
2021-09-14 18:24:33 +0200

    Add ZSH

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ REPO_ROOT="$(readlink -f $(dirname $0))"
 
 cd "${REPO_ROOT}"
 
-for config_dir in bin dunst fish git gtkrc i3 i3status ladish mpd mpv mutt offlineimap task tmux vim weechat xconfig UltiSnips; do
+for config_dir in bin dunst fish git gtkrc i3 i3status ladish mpd mpv mutt offlineimap task tmux vim weechat xconfig zsh UltiSnips; do
     ${config_dir}/bin/init.sh || true
 done
