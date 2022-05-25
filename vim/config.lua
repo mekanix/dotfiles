1771c9e99a1e9eb046eb319f8088c0dc824d8a4b
Goran Mekić <goran-mekic-tm@toyota.com>
2022-05-25 11:25:01 +0200

    Let formater/linter decide the file type

--- vim/config.lua
+++ vim/config.lua
@@ -104,8 +104,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "isort", filetypes = { "python" } },
+  { command = "black" },
+  { command = "isort" },
   {
     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
     command = "prettier",
