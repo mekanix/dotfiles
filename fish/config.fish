f71860d084ed4f850659de17c832cd1938fbfa1b
Goran Mekić <meka@tilda.center>
2026-03-03 06:11:10 +0100

    Add cargo path and make LV2/CLAP/VST3 windows float

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -x LANG "en_US.UTF-8"
 set -x LC_COLLATE "POSIX"
 set -x EDITOR nvim
-set -x PATH $PATH ~/bin ~/.local/bin ~/node_modules/.bin
+set -x PATH $PATH ~/bin ~/.local/bin ~/node_modules/.bin ~/.cargo/bin
 set -x PAGER less
 
 if status --is-interactive
