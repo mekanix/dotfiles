94d9b23003fae10a2a6cc5af9e69adb4b95aca25
Goran Mekić <meka@sys.it.com>
2026-08-29 01:52:58 +0200

    Update

--- bin/sway-startup.sh
+++ bin/sway-startup.sh
@@ -10,4 +10,3 @@ signal-desktop &
 thunderbird &
 #(discord --no-sandbox & sleep 5 && swaymsg '[class="discord"] kill') &
 (viber & sleep 5 && swaymsg '[app_id="viber"] kill') &
-(liferea & sleep 5 && swaymsg '[app_id="net.sourceforge.liferea"] kill') &
